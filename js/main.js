b75f807d6287bb29051b2c29c379d9dc2a58c56e
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2023-03-15 10:21:11 +0100

    First dev update

--- js/main.js
+++ js/main.js
@@ -1,72 +1,28 @@
-// main.js
+const title = document.querySelector('.title');
+const index = document.querySelectorAll('.index h1');
 
-const aboutMeButton = qsc('over-mij');
-const projectButton = qsc('projecten');
-const projectButtonTwo = qsc('left .projecten');
-const contactButton = qsc('contact');
-const title = document.querySelector('h1');
-const homeTitle = document.querySelector('.title');
-
-let writeText = 'portfolio van maurits';
-let i = 0;
-
-function qsc(text) {
-    return document.querySelector(`.${text}`);
-}
-
-function goTo(text) {
-    switch(text) {
-        case 'about-me':
-            window.location.href = 'over-mij.html';
-            break;
-        case 'projects':
-            window.location.href = 'projecten.html';
-            break;
-        case 'contact':
-            window.location.href = 'contact.html';
-            break;
-        case 'home':
-            window.location.href = 'index.html';
-            break;
-    }
-}
-
-function write() {
-    if (i < writeText.length) {
-        homeTitle.innerHTML += writeText.charAt(i);
-        i++;
-        setTimeout(write, 50);
-    } else {
-        i = 0;
-        setTimeout(del, 2500);
-    }
-}
-
-function del() {
-    let j = writeText.length;
-    if (i < j) {
-        i++;
-        homeTitle.innerHTML = homeTitle.innerHTML.slice(0, homeTitle.innerHTML.length - 1);
-        setTimeout(del, 50);
-    } else {
-        setTimeout(write, 50);
-        i = 0;
-        if (writeText == 'welkom :)') {
-            writeText = 'portfolio van maurits';
-        } else {
-            writeText = 'welkom :)';
+function dropIndex() {
+    let currentDelay = 0;
+    index.forEach(function(data) {
+        for (let i = 0; i < data.innerText.length; i++) {
+            setTimeout(function() {
+                data.childNodes[i].style.transform = "translateY(-10px)"
+                data.childNodes[i].style.opacity = "1"
+            }, currentDelay + i*30);
+            setTimeout(function() {
+                data.childNodes[i].style.transform = "translateY(0px)"
+            }, currentDelay + i*30 + 500);
         }
+        currentDelay += 500;
+    });
+}
+
+index.forEach(function(data) {
+    let currentString = ''
+    for (let i = 0; i < data.innerText.length; i++) {
+        currentString += `<span class="opacity letter l${i}">${data.innerText[i]}</span>`;
     }
-}
+    data.innerHTML = currentString;
+});
 
-if (homeTitle) {
-    setTimeout(write, 50);
-}
-
-aboutMeButton.addEventListener('click', function() {goTo('about-me')});
-projectButton.addEventListener('click', function() {goTo('projects')});
-if (projectButtonTwo) {
-    projectButtonTwo.addEventListener('click', function() {goTo('projects')});
-}
-contactButton.addEventListener('click', function() {goTo('contact')});
-title.addEventListener('click', function() {goTo('home')});
+title.addEventListener('click', dropIndex)
